d01171de46638cc757056f549f6300c15f050eb5
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-11-22 16:46:54 +0000

    Update +page.svelte
    Just testing the imports.

--- ledger-wallet/src/routes/+page.svelte
+++ ledger-wallet/src/routes/+page.svelte
@@ -4,8 +4,10 @@
 	import TransactionsList from "$lib/TransactionsList.svelte";
 	import SendTokens from "$lib/SendTokens.svelte";
 	import WalletInformation from "$lib/WalletInformation.svelte";
-    import {WalletAddress, IsWalletConnected} from "$lib/store";
+    import {WalletAddress, IsWalletConnected, PhantasmaRPC} from "$lib/store";
 	import WalletConnection from "$lib/WalletConnection.svelte";
+	import { onMount } from "svelte";
+	import type { Account, PhantasmaAPI } from "phantasma-ts";
     let walletAddress = "asdasd";
     let isLedgerSupported = false;
 </script>
